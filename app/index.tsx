b49cdd630c69070788c5dacdb05d3faf23035e33
Yashika118 <kumariyashika78@gmail.com>
2024-10-17 18:16:47 +0530

    create GroupList Component

--- app/index.tsx
+++ app/index.tsx
@@ -1,23 +1,17 @@
-import React from 'react';
-import { StyleSheet, Text, View,FlatList} from 'react-native';
+import React, { useState } from 'react';
+import { StyleSheet, Text, View, FlatList } from 'react-native';
 import HomePageHeader from '@/components/HomePageHeader';
-
+import GroupList from "../components/GroupList";
 
 export default function Index() {
 
-  const TaskGroups = [
-    { id: '1', title: 'Website for Rune.io',  tasks: '12 Tasks', progress: 0.4 },
-    { id: '2', title: 'Dashboard for ProSavvy',  tasks: '12 Tasks', progress: 0.75 },
-    { id: '3', title: 'Mobile Apps for Track.id',  tasks: '12 Tasks', progress: 0.5 },
-    { id: '4', title: 'Website for CourierGo.com', tasks: '12 Tasks', progress: 0.4 },
-  ];
- 
-
   return (
     <View style={styles.container} >
-      <HomePageHeader/>
+      <HomePageHeader />
+
+      
       <View>
-        
+        <GroupList/>
       </View>
     </View>
   );
@@ -27,12 +21,6 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#c9e6e4',
-  
-  },
-  text: {
-    color: 'white',
-  },
 
-  
-
+  },
 });
