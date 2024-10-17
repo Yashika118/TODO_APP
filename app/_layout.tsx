72e0da2eb8c16f8f486b3259ba50a356866ad1e5
Yashika118 <kumariyashika78@gmail.com>
2024-10-17 14:44:32 +0530

    create all necessary screens

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -3,7 +3,11 @@ import { Stack } from "expo-router";
 export default function RootLayout() {
   return (
     <Stack>
-      <Stack.Screen name="index" options={{headerTitle:"Home"}}/>
+      <Stack.Screen name="index" options={{headerTitle:"Home" }}/>
+      <Stack.Screen name="TodoList" options={{headerTitle:"TodoList"}}/>
+      <Stack.Screen name="TaskDetail" options={{headerTitle:"TaskDetail"}}/>
+      <Stack.Screen name="TaskCreation" options={{headerTitle:"TaskCreation"}}/>
+      <Stack.Screen name="+not-found" options={{headerTitle:"Oops ! Not Found", headerBackVisible:false}}/>
     </Stack>
   );
 }
--- app/index.tsx
+++ app/index.tsx
@@ -1,15 +1,26 @@
-import { Text, View } from "react-native";
+import { Text, View, StyleSheet } from "react-native";
+import { Link } from "expo-router";
 
 export default function Index() {
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
+    <View style={styles.container} >
+      <Text style={styles.text}>Home screen.</Text>
+      <Link href="./TodoList" style={styles.text}> TodoList Screen.</Link>
+      <Link href="./TaskDetail" style={styles.text}>TaskDetail Screen</Link>
+      <Link href="./TaskCreation" style={styles.text}>TaskCreation Screen</Link>
+      <Link href="./Task345Creation" style={styles.text}>NotFound Screen</Link>
     </View>
   );
 }
+
+const styles=StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#64015b',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  text: {
+    color: 'white',
+  },
+});
